e00a1e08651c98ce9f620fa5f6cb19855dfe3b21
pieteradejong <pieter.a.dejong@gmail.com>
2014-02-02 23:38:58 -0800

    expanded mainController

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,6 +1,14 @@
 app.controller("mainController", function($scope, $http){
 
+    $scope.apiKey = "";
     $scope.init = function() {
+      var today = new Date();
+      var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
+        $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
+            console.log(data);
+        }).error(function(error) {
+ 
+        });
 
     };
 
